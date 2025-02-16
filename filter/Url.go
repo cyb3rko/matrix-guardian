661d4d7b5fc44e29fca4ee4609cf6fd4827fad34
Niko Diamadis <niko@cyb3rko.de>
2025-02-16 20:55:16 +0100

    feat: allow uppercase chars in URLs

--- filter/Url.go
+++ filter/Url.go
@@ -7,10 +7,11 @@ import (
 	"strings"
 )
 
-const RegexUrl = "[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?"
+const RegexUrl = "[a-zA-Z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?"
 
 func IsUrlFiltered(database *sql.DB, urls []string) bool {
 	for _, u := range urls {
+		u = strings.ToLower(u)
 		if !strings.HasPrefix(u, "http") {
 			u = "http://" + u
 		}
